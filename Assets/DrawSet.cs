2c73b3a6e52130066766865f4ea73d013a616b6f
JoaoPito <joao.pito@hotmail.com>
2020-02-12 21:45:01 +0000

    12/02/20 initial commit

---
+++ Assets/DrawSet.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class DrawSet : MonoBehaviour
+{
+    public Texture2D setTexture;
+    public RawImage targetObj;
+
+    public List<Color> colorList = new List<Color>();
+    public Vector2 textureSize = new Vector2(512,512);
+
+    public MainScr mainScr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DrawButton()
+    {
+        Vector2 stepSize = new Vector2( (Mathf.Abs(mainScr.realSpace.x) + Mathf.Abs(mainScr.realSpace.y)) / textureSize.x, 
+                                        (Mathf.Abs(mainScr.complexSpace.x) + Mathf.Abs(mainScr.complexSpace.y)) / textureSize.y);
+        mainScr.stepSize = stepSize;
+
+        mainScr.CalculateSet();
+        StartDrawing();
+    }
+
+    public void StartDrawing()
+    {
+        colorList = new List<Color>();
+        //textureSize.x = Mathf.Sqrt(mainScr.set.Count);
+        //textureSize.y = Mathf.Sqrt(mainScr.set.Count);
+
+        Texture2D tex = new Texture2D(Mathf.FloorToInt(textureSize.x), Mathf.FloorToInt(textureSize.y));
+
+        for (int i = 0; i < mainScr.set.Count; i++)
+        {
+            if (mainScr.set[i].y < 2)
+            {
+                colorList.Add(new Color(mainScr.set[i].y / 2,0,0));
+            }
+            else colorList.Add(new Color((mainScr.set[i].z / mainScr.iterations)+0.1f, (mainScr.set[i].z / mainScr.iterations)/2, 1));
+        }
+
+
+        for(int y = 0; y < textureSize.y; y++)
+        {
+            for (int x = 0; x < textureSize.x; x++)
+            {
+                tex.SetPixel(x, y, colorList[x + (y* Mathf.FloorToInt(textureSize.x))]);                
+            }
+        }
+
+        tex.Apply();
+        setTexture = tex;
+        byte[] bytes = tex.EncodeToPNG();
+        //Object.Destroy(tex);
+
+        File.WriteAllBytes(Application.dataPath + "/../Assets/SavedScreen.png", bytes);
+
+        targetObj.texture = tex;
+    }
+}
---
+++ Assets/MainScr.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScr : MonoBehaviour
+{
+    public bool running = false;
+    Vector2 c;
+    Vector2 z;
+
+    public Vector2 stepSize = new Vector2(0.1f,0.1f);
+    public int iterations = 100;
+
+    public Vector2 realSpace = new Vector2(-1, 1);
+    public Vector2 complexSpace = new Vector2(-1, 1);
+
+    public List<Vector3> set;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void CalculateSet()
+    {
+        set = new List<Vector3>();
+
+        for(c.y = complexSpace.x; c.y < complexSpace.y; c.y += stepSize.y)
+        {
+            for(c.x = realSpace.x; c.x < realSpace.y; c.x += stepSize.x)
+            {
+                z  = Vector2.zero;
+                int i = 0;
+
+                for (int j = 0; j < iterations; j++)
+                {
+                    Vector2 newZ = Vector2.zero;
+                    newZ.x = Mathf.Pow(z.x, 2) - Mathf.Pow(z.y, 2) + c.x;//Parte real(x)
+                    newZ.y = 2*(z.x*z.y) + c.y;//Parte Imaginaria(yi)
+
+                    z = newZ;
+
+                    i = j;
+
+                    if (Mathf.Pow(z.x, 2) + Mathf.Pow(z.y, 2) > 4)
+                    {
+                        j += iterations;
+                    }
+                }
+
+                //Debug.Log("c: " + c + " | z: " + z + " sqrt: "+ Mathf.Sqrt(Mathf.Pow(z.x, 2) + Mathf.Pow(z.y, 2)) + " | i: " + i);
+                set.Add(new Vector3(Mathf.Sqrt(Mathf.Pow(c.x, 2) + Mathf.Pow(c.y, 2)), Mathf.Sqrt(Mathf.Pow(z.x, 2) + Mathf.Pow(z.y, 2)), i));
+            }
+            
+        }
+    }
+}
